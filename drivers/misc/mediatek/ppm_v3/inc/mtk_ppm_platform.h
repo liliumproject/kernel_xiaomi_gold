ee520a03820fba472603a0937883734eb4dd1390
HeroBuxx <me@herobuxx.me>
2024-10-06 11:00:11 +0000

    drivers: misc: mediatek: ppm_v3: inc: Import Xiaomi changes
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ drivers/misc/mediatek/ppm_v3/inc/mtk_ppm_platform.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 MediaTek Inc.
+ */
+
+#ifndef __MT_COMMON_PPM_PLATFORM_H__
+#define __MT_COMMON_PPM_PLATFORM_H__
+
+#if IS_ENABLED(CONFIG_MTK_PLAT_POWER_MT6765)
+#include <mtk_ppm_platform_6765.h>
+#elif IS_ENABLED(CONFIG_MTK_PLAT_POWER_MT6833)
+#include <mtk_ppm_platform_6833.h>
+#elif IS_ENABLED(CONFIG_MTK_PLAT_POWER_MT6768)
+#include <mtk_ppm_platform_6768.h>
+#endif
+
+#endif
